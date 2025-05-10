c86797bb4cdbaf3337b35b2737e90a3043fee36c
Yashr22 <115694188+Yashr22@users.noreply.github.com>
2025-05-10 22:15:33 +0530

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, UploadFile # type: ignore
+import uvicorn # type: ignore
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf# type: ignore
+from fastapi.middleware.cors import CORSMiddleware# type: ignore
+from fastapi.responses import JSONResponse# type: ignore
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can restrict this to your frontend URL later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model('Trained_model/cnn_plant_disease_model.h5')
+# MODEL = tf.keras.models.load_model('Trained_model/glitch.h5')
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy", "Unknown"]
+
+def read_file_as_image(data) -> np.ndarray:
+    image = Image.open(BytesIO(data)).convert("RGB")
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    predicted_index = np.argmax(predictions[0])
+
+    if confidence < 0.9:
+        predicted_class = "Unknown"
+    else:
+        predicted_class = CLASS_NAMES[predicted_index]
+
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
